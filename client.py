3173c78052e8880b206f4b2631ef5deb482912b8
LochTech <lochtechmonster@gmail.com>
2020-11-11 17:29:09 +1100

    Some stuff already done

---
+++ client.py
@@ -0,0 +1,73 @@
+#coding: utf-8
+from socket import *
+from sys import argv
+from typing import final
+
+user = ''
+
+#FIXME: Remove response print
+
+def typeError(str):
+    print(str)
+
+def typeMessage(str):
+    print(str)
+
+def typeInput(str):
+    response = input(str)
+    sendToServer(response)
+
+def recvFromServer():
+    response = clientSocket.recv(1024).decode('utf-8')
+    print('response: ' + response)
+    commands = response.split('-*-')
+    DEBUG_i = 0
+    for cmd in commands:
+        if cmd == '':
+            # Last command
+            break
+        print(DEBUG_i)
+        command = cmd[0]
+        if command == 'E':
+            typeError(cmd[1:])
+        elif command == 'M':
+            typeMessage(cmd[1:])
+        elif command == 'I':
+            typeInput(cmd[1:])
+        elif command == 'U':
+            global user
+            user = cmd[1:]
+        elif command == 'L':
+            # Logout
+            logout()
+        DEBUG_i += 1
+
+def logout():
+    clientSocket.close()
+    exit()
+
+def sendToServer(str):
+    clientSocket.send((user + ' ' + str).encode('utf-8'))
+
+try:
+    if __name__ == "__main__":
+        serverName = 'localhost'
+        serverPort = 12000
+
+        if len(argv) == 3:
+            serverName = argv[1]
+            serverPort = int(argv[2])
+        else:
+            print('Correct usage: python3 client.py server_IP server_port')
+
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        #This line creates the client’s socket. The first parameter indicates the address family; in particular,AF_INET indicates that the underlying network is using IPv4. The second parameter indicates that the socket is of type SOCK_STREAM,which means it is a TCP socket (rather than a UDP socket, where we use SOCK_DGRAM). 
+        clientSocket.connect((serverName, serverPort))
+        #Before the client can send data to the server (or vice versa) using a TCP socket, a TCP connection must first be established between the client and server. The above line initiates the TCP connection between the client and server. The parameter of the connect( ) method is the address of the server side of the connection. After this line of code is executed, the three-way handshake is performed and a TCP connection is established between the client and server.
+
+        while True:
+            recvFromServer()
+except KeyboardInterrupt:
+    pass
+finally:
+    logout()
---
+++ server.py
@@ -0,0 +1,483 @@
+#coding: utf-8
+from socket import *
+from sys import argv
+from os import path, SEEK_END#, remove
+import re
+
+#FIXME: Remove line numbers from responses
+'''
+Initial Connection: 
+When connecting, client prompts for a username
+ - Receive username
+ - Check credentials.txt for a match
+If name exists send confirmation to client
+ - Client prompts for password
+ - Receive password
+If password match then confirm to client
+If doesn't then send error to client
+
+If things don't match then:
+ - Client prompts for username
+ - If doesn't exist, prompt for new password
+Creates new line in credentials.txt
+ - Send confirmation to client
+ - Client sends message to user
+Make sure credentials.txt has write permissions
+
+Multi-threading version:
+Keep track of logged in accounts
+Make sure connecting users aren't using the same account
+
+
+Forum Ops:
+Commands are sent as uppercase
+Arguments are separated by one space and will be one word long
+  Except Messages which can contain whitespace
+If anything doesn't follow usage specs then send error to user
+  User prompted to pick a command again
+Client sends username on every command
+
+11 Different commands
+
+CRT: Create thread
+USAGE: 'CRT threadtitle'
+Thread titles are one word long
+Client sends command, title of thread and username
+each thread is a text file with the name 'threadtitle' (no .txt)
+First line is the username of who created the thread
+The rest of the lines are messages, added in chronological order (just append to the end)
+Server checks if a thread exists with the same title (sends error)
+If doesn't exist, create file
+Send confirmation to client
+
+MSG: Post Message
+USAGE: 'MSG threadtitle message'
+The message can contain spaces
+If a thread with the title doesn't exist, send error
+Append message and username to thread file
+'messagenumber username: message'
+Messages start at 1
+Send confirmation to client
+
+DLT: Delete message
+USAGE: 'DLT threadtitle messagenumber'
+Message can only be deleted by the user who posted the message
+Check:
+  Thread exists
+  Message number exists
+  If user sent message
+Delete line in thread
+  Remove line in the file
+  move all other messages up by one line
+  update message numbers appropriately
+
+EDT: Edit message
+USAGE: 'EDT threadtitle messagenumber message'
+Message can only be edited by the user who posted that message
+Message is the updated message
+Check:
+  Thread exists
+  Message number exists
+  If user sent message
+Replace original message with the replacement
+  Message number and username remain unchanged
+
+LST: List threads
+USAGE: 'LST'
+No arguments
+Lists all thread titles
+Client prints the list in the terminal
+  one thread per line
+If no threads, then give that message to user
+
+RDT: Read thread
+USAGE: 'RDT threadtitle'
+Sends the file minus the first line to the client
+Client displays all contents and info of uploaded files
+If thread doesn't exist give error
+
+UPD: Upload file
+USAGE: 'UPD threadtitle filename'
+Assume file is binary
+Client sends command and thread
+After check that thread exists
+Then sends filename and username
+Then sends file
+Stores file as 'threadtitle-filename'
+  keep current extension in name
+Assume filename is unique
+Append to thread file 'username uploaded filename'
+Those messages are sent with RDT
+
+DWN: Download file
+USAGE: 'DWN threadtitle filename'
+Client sends command, title, filename
+Check if thread exists
+Check if file is in thread
+If match, then send file
+Client saves as 'filename' no threadtitle
+Can assume client doesn't have file
+Once transfer is complete, send confirmation
+File is not deleted on server end
+
+RMV: Remove thread
+USAGE: 'RMV threadtitle'
+Check thread exists
+Check user created thread
+Thread is deleted, all files connected are deleted
+Send confirmationn
+
+XIT: Exit
+USAGE: 'XIT'
+Closes TCP connection
+Gives goodbye message to user
+Update logged in users
+
+SHT: Shutdown
+USAGE: 'SHT admin_password'
+Checks admin password
+Sends a shutdown message to all clients
+Closes all TCP messages
+Deletes all file created by the server
+  All threads
+  All uploaded files
+  credentials file
+All sockets closed
+Exits program
+
+'''
+
+def sendError(str):
+    print('ERROR: ' + str)
+    sendToSocket('E' + str)
+    #FIXME: send different messages for all errors to user and to server
+
+def sendMessage(str):
+    sendToSocket('M' + str)
+
+def sendInput(str):
+    sendToSocket('I' + str)
+
+def sendLogout():
+    sendMessage('Goodbye')
+    sendToSocket('L')
+
+def sendToSocket(str):
+    connectionSocket.send((str + '-*-').encode('utf-8'))
+
+def receiveResponse():
+    return connectionSocket.recv(1024).decode('utf-8').strip()
+
+def thread_exists(threadtitle):
+    return path.isfile(threadtitle)
+
+def user_login():
+    '''
+    Initial Connection: 
+    When connecting, client prompts for a username
+    - Receive username
+    - Check credentials.txt for a match
+    If name exists send confirmation to client
+    - Client prompts for password
+    - Receive password
+    If password match then confirm to client
+    If doesn't then send error to client
+
+    If things don't match then:
+    - Client prompts for username
+    - If doesn't exist, prompt for new password
+    Creates new line in credentials.txt
+    - Send confirmation to client
+    - Client sends message to user
+    Make sure credentials.txt has write permissions
+    '''
+    print('Client Connected')
+    sendInput('Enter your username: ')
+    username = receiveResponse()
+    file = open('credentials.txt', 'r')
+
+    u_found = False
+    login = False
+    for line in file.readlines():
+        words = line.split()
+        # FIXME:
+        #print(words[0])
+        if words[0] == username:
+            sendInput('200Enter your password: ')
+            passwd = receiveResponse()
+            if words[1] == passwd:
+                login = True
+                currUsers.append(username)
+                print(username + ' successful login')
+                sendMessage('206Successful login')
+                sendToSocket('U' + username)
+            else:
+                sendError('Incorrect password')
+                
+            break
+    
+    file.close()
+
+    if not login:
+        if not u_found:
+            sendError('216Username not found')
+        # 
+        user_register()
+        pass
+    
+    # u_exists == true
+    # check password
+def user_register():
+    # Get username
+    sendInput('227Enter your username: ')
+    username = receiveResponse()
+    file = open('credentials.txt', 'r')
+
+    u_exists = False
+    for line in file.readlines():
+        words = line.split()
+        if words[0] == username:
+            u_exists = True
+            sendInput('236Enter your password: ')
+            passwd = receiveResponse()
+            if words[1] == passwd:
+                currUsers.append(username)
+                sendMessage('240Successful login')
+                print(username + ' successful login')
+                
+            break
+    file.close()
+    # If user doesn't exist
+    # Prompt new password
+    if not u_exists:
+        file = open('credentials.txt', 'a')
+        sendMessage('249Username not found')
+        sendInput('250Enter new password: ')
+        passwd = receiveResponse()
+        file.write('\n' + username + ' ' + passwd)
+        currUsers.append(username)
+        print('256New user ' + username + ' registered')
+        sendMessage('255User Registered')
+    file.close()
+
+    sendToSocket('U' + username)
+    # Confirmation new user
+
+def create_thread(threadtitle, user):
+    '''
+    CRT: Create thread
+    USAGE: 'CRT threadtitle'
+    Thread titles are one word long
+    Client sends command, title of thread and username
+    each thread is a text file with the name 'threadtitle' (no .txt)
+    First line is the username of who created the thread
+    The rest of the lines are messages, added in chronological order (just append to the end)
+    Server checks if a thread exists with the same title (sends error)
+    If doesn't exist, create file
+    Send confirmation to client
+    '''
+    print(user + ' issued CRT command')
+
+    if thread_exists(threadtitle):
+        sendError('277Thread already exists')
+    else:
+        # thread doesn't exist yet
+        file = open(threadtitle, 'w')
+        file.write(user)
+        file.close()
+        sendMessage('283Thread ' + threadtitle + ' created')
+        print('Thread ' + threadtitle + ' created')
+
+def send_message(threadtitle, message, user):
+    '''
+    MSG: Post Message
+    USAGE: 'MSG threadtitle message'
+    The message can contain spaces
+    If a thread with the title doesn't exist, send error
+    Append message and username to thread file
+    'messagenumber username: message'
+    Messages start at 1
+    Send confirmation to client
+    '''
+    print(user + ' issued MSG command')
+    if not thread_exists(threadtitle):
+        sendError("298Thread doesn't exist")
+        return
+    msg = ' '.join(message)
+    
+    lastNum = get_lastnumber(threadtitle)
+
+    file = open(threadtitle, "a")
+    file.write('\n' + str(lastNum + 1) + ' ' + user + ': ' + msg)
+    sendMessage('313Message sent to ' + threadtitle + ' thread')
+    print(user + ' posted to ' + threadtitle + ' thread')
+
+    file.close()
+
+def is_message(line):
+    result = re.search("^[0-9]+ .+: .+", line)
+    return (result is not None)
+
+def get_msgnumber(line):
+    # Prob try to catch error here
+    return int(line.split()[0])
+
+def get_lastnumber(threadtitle):
+    file = open(threadtitle, "r")
+    lines = file.read().splitlines()
+    #print(lines)
+    #print(len(lines))
+    lastMessage = None
+    for line in reversed(lines[1:]):
+        if is_message(line):
+            lastMessage = line
+            break
+    file.close()
+
+    lastNum = 0
+    if lastMessage is not None:
+        lastNum = get_msgnumber(lastMessage)
+    
+    return lastNum
+
+def delete_message(threadtitle, msgNumber, user):
+    '''
+    DLT: Delete message
+    USAGE: 'DLT threadtitle messagenumber'
+    Message can only be deleted by the user who posted the message
+    Check:
+        Thread exists
+        Message number exists
+        If user sent message
+    Delete line in thread
+        Remove line in the file
+        move all other messages up by one line
+        update message numbers appropriately
+    '''
+    print(user + ' issued DLT command')
+    if not thread_exists(threadtitle):
+        sendError("358Thread doesn't exist")
+        return
+    
+    msgNum = int(msgNumber)
+    if msgNum < 1 or msgNum > get_lastnumber(threadtitle):
+        sendError("Message number doesn't exist")
+        return
+    
+    found = False
+    with open(threadtitle, "r+") as f:
+        thread = f.readlines()
+        f.seek(0)
+        f.truncate()
+        f.write(thread[0].strip())
+        for line in thread[1:]:
+            #print(line)
+            if is_message(line):
+                if found:
+                    # after the message
+                    msg = ' '.join(line.split()[1:])
+                    f.write('\n' + str(get_msgnumber(line) - 1) + ' ' + msg)
+                    continue
+                if get_msgnumber(line) == msgNum:
+                    if user + ':' == line.split()[1]:
+                        sendError("Message cannot be deleted")
+                        f.write('\n' + line.rstrip())
+                    found = True
+                else:
+                    # Before the message
+                    f.write('\n' + line.strip())
+            else:
+                f.write('\n' + line.strip())
+
+
+
+    #TODO: check if user posted
+    sendMessage('Line deleted')
+    
+
+
+
+def selectCommand():
+    sendInput('Enter one of the following commands: CRT, MSG, DLT, EDT, LST, RDT, UPD, DWN, RMV, XIT, SHT:')
+    resp = receiveResponse()
+    words = resp.split()
+    username = words[0]
+    if username not in currUsers:
+        sendError('User not currently logged in')
+        return True
+    cmd = words[1]
+
+    if cmd == 'CRT':
+        create_thread(words[2], username)
+    elif cmd == 'MSG':
+        send_message(words[2], words[3:], username)
+    elif cmd == 'DLT':
+        delete_message(words[2], words[3], username)
+    elif cmd == 'EDT':
+        pass
+    elif cmd == 'LST':
+        pass
+    elif cmd == 'RDT':
+        pass
+    elif cmd == 'UPD':
+        pass
+    elif cmd == 'DWN':
+        pass
+    elif cmd == 'RMV':
+        pass
+    elif cmd == 'XIT':
+        sendMessage('Logging out')
+        sendLogout()
+        print(username + ' logged out')
+        currUsers.remove(username)
+        return False
+    elif cmd == 'SHT':
+        shutdown()
+    return True
+
+def shutdown():
+    sendMessage('Server shutting down')
+    sendLogout()
+    print('Shutting down')
+    # TODO: Remove all files and stuff
+    # TODO: close all current connections
+    connectionSocket.close()
+    serverSocket.close()
+    exit()
+
+
+currUsers = []
+
+#try:
+if __name__ == "__main__":
+    #using the socket module
+
+    #Define connection (socket) parameters
+    #Address + Port no
+    #Server would be running on the same host as Client
+    # change this port number if required
+    server_port = 12000
+    admin_passwd = None 
+
+    if len(argv) == 3:
+        server_port = int(argv[1])
+        admin_passwd = argv[2]
+    else:
+        print("Correct usage: python3 server.py server_port admin_passwd")
+        exit()
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('localhost', server_port))
+    serverSocket.listen(1)
+    print("The server is ready to receive")
+
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        user_login()
+        while selectCommand():
+            pass
+
+        connectionSocket.close()
+# except KeyboardInterrupt:
+#     pass
+# finally:
+#     shutdown()
